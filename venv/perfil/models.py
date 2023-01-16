0493d7047e6fa9cee2f535deb07acef7da7045c9
Carlos Roberto <carlosrjhoe@gmail.com>
2023-01-16 20:03:51 -0300

    Criando model do perfil de usuários

--- venv/perfil/models.py
+++ venv/perfil/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import ValidationError
+import re
+from utils.validacpf import valida_cpf
 
 # Create your models here.
 
 class Perfil(models.Model):
-    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Usuário')
     idade = models.PositiveIntegerField()
     data_nascimento = models.DateField()
     cpf = models.CharField(max_length=11)
-    endereco = models.CharField(max_length=50)
-    numero = models.CharField(max_length=5)
+    endereco = models.CharField(max_length=50, verbose_name='Endereço')
+    numero = models.CharField(max_length=5, verbose_name='Número')
     complemento = models.CharField(max_length=30)
     bairro = models.CharField(max_length=30)
     cep = models.CharField(max_length=8)
@@ -47,3 +50,23 @@ class Perfil(models.Model):
             ('TO', 'Tocantins'),
         )
     )
+
+    def __str__(self) -> str:
+        return f'{self.usuario.first_name} - {self.usuario.last_name}'
+
+    def clean(self) -> None:
+        """Validação dos campos"""
+        erro_messages = {}
+        
+        if not valida_cpf(self.cpf):
+            erro_messages['cpf'] = 'Digite um CPF válido.'
+
+        if re.search(r'[^0-9]', self.cep) or len(self.cep) < 8:
+            erro_messages['cep'] = 'CEP inválido. Digite apenas números.'
+
+        if erro_messages:
+            raise ValidationError(erro_messages)
+
+    class Meta:
+        verbose_name = 'Perfil'
+        verbose_name_plural = 'Perfis'
