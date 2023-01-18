f8b0bc5fb1104bbf338de4cbd6a3d44ba430f003
Carlos Roberto <carlosrjhoe@gmail.com>
2023-01-18 20:31:10 -0300

    Configurando as URLS, peido, perfil, produto

--- venv/pedido/views.py
+++ venv/pedido/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views import View
-from django.http import HttpResponse
 
 # Create your views here.
 class Pagar(View):
