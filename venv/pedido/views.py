512c852bcf8550d0be74979c4aa0adab0eaf172a
Carlos Roberto <carlosrjhoe@gmail.com>
2023-01-18 21:54:25 -0300

    Configurando as URLS dos apps PEDIDO, PERFIL, PRODUTO

--- venv/pedido/views.py
+++ venv/pedido/views.py
@@ -2,14 +2,11 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.views import View
 
+
 # Create your views here.
 class Pagar(View):
     pass
-
-
 class FecharPedido(View):
     pass
-
-
 class Detalhe(View):
     pass
