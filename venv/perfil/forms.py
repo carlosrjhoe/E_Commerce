f630650f43d9d88a27b86f125a717b2ca9df7a96
Carlos Roberto <carlosrjhoe@gmail.com>
2023-02-10 21:33:47 -0300

    Encontrei um bug de csrf_token no perfil.html

--- venv/perfil/forms.py
+++ venv/perfil/forms.py
@@ -15,6 +15,11 @@ class UserForm(forms.ModelForm):
         widget=forms.PasswordInput(),
         label='Senha'
     )
+    password_2 = forms.CharField(
+        required=False,
+        widget=forms.PasswordInput(),
+        label='Confirmação da Senha'
+    )
     
     def __init__(self, usuario=None, *args, **kwargs):
         return super().__init__(*args, **kwargs)
@@ -28,5 +33,40 @@ class UserForm(forms.ModelForm):
     def clean(self, *args, **kwargs):
         data = self.data
         cleaned = self.cleaned_data
+        validation_error_msg = {}
 
-        
+        usuario_data = cleaned.get('username')
+        email_data = cleaned.get('email')
+        password_data = cleaned.get('password')
+        password2_data = cleaned.get('password2')
+
+        usuario_db = User.objects.filter(username=usuario_data).first()
+        email_db = User.objects.filter(email=email_data).first()
+
+        error_msg_user_exists = 'Usuário já existe.'
+        error_msg_email_exists = 'E-mail já existe.'
+        error_msg_password_match = 'As duas senhas não conferem.'
+        error_msg_password_short = 'Sua senha precisa ter 6 caracteres...'
+
+        if self.usuario:
+            if usuario_data != usuario_db.username:
+                if usuario_db:
+                    validation_error_msg['username'] = error_msg_user_exists
+                    
+                if email_data != email_db.email:
+                    validation_error_msg['email'] = error_msg_email_exists
+                    
+                if password_data:
+                    if password_data != password2_data:
+                        validation_error_msg['password'] = error_msg_password_match
+                        validation_error_msg['password2'] = error_msg_password_match
+                    if len(password_data):
+                        if email_db:
+                            validation_error_msg['password'] = error_msg_password_short
+        else:
+            pass
+
+        if validation_error_msg:
+            raise(forms.ValidationError(validation_error_msg))
+        else:
+            pass
